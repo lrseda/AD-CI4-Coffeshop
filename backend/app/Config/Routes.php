cf6d0f6d1f52b49ace9df18c4a714058ecac404b
lrseda <lrseda@fit.edu.ph>
2025-09-21 14:57:05 +0000

    feat(backend): Add route for Moodboard page

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Users::index');
 $routes->get('/Login', 'Login::index');
 $routes->get('/Signup', 'Signup::index');
+$routes->get('/Moodboard', 'Moodboard::index');
